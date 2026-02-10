bb64b6749ca99e1e9c4d88755b057385b2c649bf
vasim katiya <vasimkatiya97@gmail.com>
2026-02-10 20:22:49 +0530

    database bug fixing

--- db/pool.js
+++ db/pool.js
@@ -2,12 +2,10 @@ require('dotenv').config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  // host: process.env.DB_HOST,
-  // user: process.env.DB_USER,
-  // password:process.env.DB_PASSWORD,
-  // database:process.env.DB_NAME,
-  // port: process.env.DB_PORT
-  connectionString:process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 
--- db/populateDatabase.js
+++ db/populateDatabase.js
@@ -60,12 +60,10 @@ ON CONFLICT (anime_name) DO NOTHING;
 
 async function main() {
 const client = new Client({
-  // host: process.env.DB_HOST,
-  // user: process.env.DB_USER,
-  // password: process.env.DB_PASSWORD,
-  // database: process.env.DB_NAME,
-  // port: process.env.DB_PORT
-  connectionString:process.env.DATABASE_URL
+   connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
 });
 
        
