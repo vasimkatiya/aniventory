180f0789b1eec36fef0d4784abfbbf5cb049e46c
vasim katiya <vasimkatiya97@gmail.com>
2026-02-10 14:56:53 +0530

    add DB_URL in .env file

--- db/populateDatabase.js
+++ db/populateDatabase.js
@@ -60,11 +60,12 @@ ON CONFLICT (anime_name) DO NOTHING;
 
 async function main() {
 const client = new Client({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  // host: process.env.DB_HOST,
+  // user: process.env.DB_USER,
+  // password: process.env.DB_PASSWORD,
+  // database: process.env.DB_NAME,
+  // port: process.env.DB_PORT
+  connectionString:process.env.DB_URL
 });
 
        
